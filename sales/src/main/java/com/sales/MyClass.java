d547c23deadf036a16c61176b68199e0e333b0ea
nangudeprashant <30728347+nangudeprashant@users.noreply.github.com>
2018-01-08 18:18:44 +0530

    Second commit.

--- sales/src/main/java/com/sales/MyClass.java
+++ sales/src/main/java/com/sales/MyClass.java
@@ -4,5 +4,6 @@ public class MyClass {
 public static void main(String[] args) {
 	System.out.println("Hello World!!!!!");
 	System.out.println("Added in first commit!!!!!");
+	System.out.println("Added in second commit!!!!!");
 }
 }
