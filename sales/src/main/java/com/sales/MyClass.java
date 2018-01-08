82370f95a23d1ea8568e48bca79f21137bcdd52f
nangudeprashant <30728347+nangudeprashant@users.noreply.github.com>
2018-01-08 18:03:28 +0530

    First commit.

---
+++ sales/src/main/java/com/sales/MyClass.java
@@ -0,0 +1,8 @@
+package com.sales;
+
+public class MyClass {
+public static void main(String[] args) {
+	System.out.println("Hello World!!!!!");
+	System.out.println("Added in first commit!!!!!");
+}
+}
